1debeb68d1439d3859ae237436d7c129c6a4c6ed
Nitamayega <ntmygaa@gmail.com>
2023-04-13 10:40:20 +0700

    add delete & edit

--- resources/views/app/wali_siswa/edit.blade.php
+++ resources/views/app/wali_siswa/edit.blade.php
@@ -1,4 +1,70 @@
 @extends('layouts.master')
 @section('content')
-
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Basic form control</h5>
+                </div>
+                <form class="form theme-form" method="POST" action="{{route('wali.update', $data->id_wali)}}">
+                    @csrf
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">Nama</label>
+                                    <input class="form-control" type="text"
+                                        value="{{ $data->nama }}" name="nama">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">No. Telepon</label>
+                                    <input class="form-control" type="tel"
+                                        value="{{ $data->no_telp }}" name="no_telp">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">NIM</label>
+                                    <input class="form-control" type="text"
+                                        value="{{ $data->nim }}" name="nim">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">Alamat</label>
+                                    <textarea class="form-control" name="alamat">{{ $data->alamat }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <label for="">Hubungan</label>
+                                    <select class="form-control" name="hubungan" value="{{ $data->hubungan }}">
+                                        <option>Ayah</option>
+                                        <option>Ibu</option>
+                                        <option>Saudara Kandung</option>
+                                        <option>Lainnya</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                        <button class="btn btn-pill btn-primary" type="submit">Submit</button>
+                        <a href='{{ url('wali/index') }}' class="btn btn-pill btn-secondary">Cancel</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -42,15 +42,19 @@ Route::get('/wali/index', [WaliSiswaController::class, 'index'])->name('wali.ind
 Route::get('/wali/create', [WaliSiswaController::class, 'create'])->name('wali.create');
 Route::post('/wali/store', [WaliSiswaController::class, 'store'])->name('wali.store');
 Route::get('/wali/edit/{id}', [WaliSiswaController::class, 'edit'])->name('wali.edit');
+Route::post('/wali/update/{id}', [WaliSiswaController::class, 'update'])->name('wali.update');
+Route::delete('/wali/delete/{id}', [WaliSiswaController::class, 'destroy'])->name('wali.destroy');
+
 //Absensi
 Route::get('/absensi', [AbsensiController::class, 'index'])->name('absensi.index');
 Route::get('/absensi/create', [AbsensiController::class, 'create'])->name('absensi.create');
 
-//senior resident
+//Senior Resident
 Route::get('/senior_resident/index', [SRController::class, 'index'])->name('sr.index');
 Route::get('/senior_resident/create', [SRController::class, 'create'])->name('sr.create');
 Route::post('/senior_resident/store', [SRController::class, 'store'])->name('sr.store');
 
+//Gedung
 Route::get('/gedung/index', [GedungController::class, 'index'])->name('gedung.index');
 Route::get('/gedung/create', [GedungController::class, 'create'])->name('gedung.create');
 Route::post('/gedung/store', [GedungController::class, 'store'])->name('gedung.store');
